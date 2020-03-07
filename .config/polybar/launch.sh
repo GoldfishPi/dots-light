27a230209eddd9ae9c22fa6d64df804bebf2b495
Erik Badger <erikbadger777@gmail.com>
2020-03-07 17:50:32 -0600

    updated everything to be prettier lol

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
@@ -7,7 +8,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar main >>/tmp/polybar1.log 2>&1 &
+polybar white &
+#polybar bar2 &
 
 echo "Bars launched..."
