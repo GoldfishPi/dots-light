313afe3e07c977cd0e0bc426b5748f619c79ac6e
Erik Badger <erikbadger777@gmail.com>
2020-09-08 09:27:53 -0500

    added network and memory to bar

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -12,7 +12,7 @@ Config {
    -- layout
    , sepChar =  "%"   -- delineator between plugin names and straight text
    , alignSep = "}{"  -- separator between left-right alignment
-   , template = "%battery% %StdinReader% }{ %date% "
+   , template = "%battery% %StdinReader% }{ [ %dynnetwork% ] [ %memory% ] [ %date% ]"
 
    -- general behavior
    , lowerOnStart =     True    -- send to bottom of window stack on start
