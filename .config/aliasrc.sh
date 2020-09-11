78593cd098a78b8785c78e7667cac37bcd3d2590
Erik Badger <erikbadger777@gmail.com>
2020-09-11 14:33:11 -0500

    using exa for ls

--- .config/aliasrc.sh
+++ .config/aliasrc.sh
@@ -3,6 +3,8 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias vim='nvim'
 alias g='git'
 
+alias ls='exa --color=always --group-directories-first'
+
 alias tw='task project:Work'
 alias th='task project:Home'
 
