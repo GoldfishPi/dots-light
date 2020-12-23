b535cce72a680627c31c19eb70fcfe03c0416996
Erik Badger <erikbadger777@gmail.com>
2020-12-23 14:55:58 -0600

    (feature) enabled rls language server

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,5 +1,6 @@
 
 require'lspconfig'.tsserver.setup{ on_attach=require'completion'.on_attach }
+require'lspconfig'.rls.setup{ on_attach=require'completion'.on_attach }
 
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
