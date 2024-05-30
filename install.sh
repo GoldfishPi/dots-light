1c0542325b2ed685998a4e778fe3479c08b9dd83
Erik Badger <erikbadger777@gmail.com>
2024-05-30 15:32:34 +0000

    feat: added change shell

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 
 sudo pacman -S --needed starship zsh tmux neovim
 
+if [ ! "$SHELL" == "/bin/zsh" ]; then
+        chsh -s /bin/zsh
+fi
+
 if [ ! -d "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim" ]; then
         git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
         nvim -c 'PackerInstall'
@@ -15,3 +19,4 @@ if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
 fi
 
 yay -S --needed nvim-packer-git ttf-firacode-nerd
+git config --global core.editor "nvim"
