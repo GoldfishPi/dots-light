c64ef668d9a8e03d1e24a7113af5b87ffd493c7f
Erik Badger <erikbadger777@gmail.com>
2024-05-30 15:47:00 +0000

    feat: added exa and bat to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 
-sudo pacman -S --needed starship zsh tmux neovim docker
+sudo pacman -S --needed starship zsh tmux neovim docker exa bat
 
 if [ ! "$SHELL" == "/bin/zsh" ]; then
         chsh -s /bin/zsh
