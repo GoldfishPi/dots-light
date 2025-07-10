bb51fcc0f6b4e0fc52344c853fdc623447fa2214
Erik Badger <erikbadger777@gmail.com>
2025-07-10 14:47:52 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 
-sudo pacman -S --needed --noconfirm starship zsh tmux neovim docker exa bat fastfetch sway swaybg fuzzel nemo kitty nodejs npm mako
+sudo pacman -S --needed --noconfirm starship zsh tmux neovim docker exa bat fastfetch sway swaybg fuzzel nemo kitty nodejs npm mako git base-devel
 
-if [ ! "$SHELL" == "/bin/zsh" ]; then
-        chsh -s /bin/zsh
-fi
+# if [ ! "$SHELL" == "/bin/zsh" ]; then
+#         chsh -s /bin/zsh
+# fi
 
 if [ ! -e "/bin/yay" ]; then
-        sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+        git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si --noconfirm
 fi
 
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
@@ -19,5 +19,9 @@ sudo systemctl enable docker.service
 
 yay -S --needed --noconfirm nvim-packer-git ttf-firacode-nerd
 git config --global core.editor "nvim"
+
+if [ ! -d "./.config" ]; then
+  mkdir .config
+fi
 
 touch ./.config/tokens.sh
